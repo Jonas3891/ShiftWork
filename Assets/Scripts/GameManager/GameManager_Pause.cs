499530c735bfc750b13231178d355b51c9891b72
John B <jbuckl8@uwo.ca>
2021-03-23 13:15:11 -0400

    Created GameManger and Pause script

---
+++ Assets/Scripts/GameManager/GameManager_Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager_Pause : MonoBehaviour
+{
+
+    private GameManager gameManagerMaster;
+    private bool isPaused;
+
+    private void OnEnable()
+    {
+        SetInitialReference();
+        gameManagerMaster.menuToggleEvent += TogglePause;
+    }
+
+    private void OnDisable()
+    {
+        gameManagerMaster.menuToggleEvent -= TogglePause;
+    }
+
+    void SetInitialReference()
+    {
+        gameManagerMaster = GetComponent<GameManager>();
+    }
+
+    void TogglePause()
+    {
+        if(isPaused){
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+}
