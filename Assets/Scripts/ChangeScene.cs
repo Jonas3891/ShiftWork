91cf622a1d154fcb39b20eb7d198053b1b3143bb
John B <jbuckl8@uwo.ca>
2021-03-25 14:27:47 -0400

    Updated the scenes. Now the game will go from the Main Menu (MainMenu) to a short opening CutScene (OpeningScene) then switches to the gameplay scene (CityPartial)

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    GameObject sceneToLoad;
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene("CityPartial");
+    }
+}
