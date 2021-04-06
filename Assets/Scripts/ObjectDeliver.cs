9ef02c48ad96639d196496e34703c971fd677392
John B <jbuckl8@uwo.ca>
2021-04-05 21:04:00 -0400

    p

---
+++ Assets/Scripts/ObjectDeliver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDeliver : MonoBehaviour
+{
+    public delegate void pop();
+    public static event pop OnPop;
+    private int objective;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "pickUp")
+        {
+            Destroy(other.gameObject);
+            OnPop();
+
+        }
+    }
+
+
+}
