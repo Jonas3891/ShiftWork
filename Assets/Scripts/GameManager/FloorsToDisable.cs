d6a392d3589c07ea68c3ed7d0f164b9194b24c72
John B <jbuckl8@uwo.ca>
2021-04-05 20:14:54 -0400

    disables floors

---
+++ Assets/Scripts/GameManager/FloorsToDisable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorsToDisable : MonoBehaviour
+{
+    GameObject Floor1;
+    GameObject Floor2;
+    GameObject Floor3;
+    GameObject Floor4;
+    GameObject Floor5;
+    GameObject Floor6;
+
+
+}
